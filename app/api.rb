f7917d180d610aff36625d32955a7399ed87ad1d
John Robert Mahlan <johnrobertmahlan@gmail.com>
2021-11-01 19:43:22 -0600

    annotate routes and use rackup to boot server

--- app/api.rb
+++ app/api.rb
@@ -3,10 +3,16 @@ require 'json'
 
 module ExpenseTracker
   class API < Sinatra::Base
+    # this method is just creating a dummy hash with the same key every time
+    # once we hook up a DB, we'll update this so that we can post real expenses
     post '/expenses' do
       JSON.generate('expense_id' => 42)
     end
 
+    # right now, this method is kind of useless because we're not persisting any requests
+    # since there's no data to get, we're going to keep failing our specs
+    # so, we need to find a way to persist expenses in a DB
+    # then we can grab them using params
     get '/expenses/:date' do
       JSON.generate([])
     end
