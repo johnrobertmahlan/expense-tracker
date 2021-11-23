2a6a06efe94bdfc0fa6249cf3a9642837a2cafbe
John Robert Mahlan <johnrobertmahlan@gmail.com>
2021-11-22 20:40:10 -0700

    add basic error handling

--- app/ledger.rb
+++ app/ledger.rb
@@ -38,7 +38,13 @@ module ExpenseTracker
       # REMEMBER that the expense is being passed as a param here
       # it's RETRIEVED in the POST method in api.rb, where it comes from the request body
       # so all we have to do here is record the expense to the DB
+      # UPDATE: we have to handle bad expenses, so the logic will get more complicated
 
+      unless expense.key?('payee')
+        message = 'Invalid expense: `payee` is required'
+        return RecordResult.new(false, nil, message)
+      end
+      
       DB[:expenses].insert(expense)
       id = DB[:expenses].max(:id) # the ids will be sequential, so the HIGHEST id will be the NEWEST expense
       RecordResult.new(true, id, nil)
